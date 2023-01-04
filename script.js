6cb9c47f2c942632667f63016dfa6a37ae8dd3ea
Keerthanaparthiban <92458543+Keerthanaparthiban@users.noreply.github.com>
2023-01-04 11:39:47 +0530

    illustration 3

---
+++ script.js
@@ -0,0 +1,12 @@
+/*const navSlide = () => {
+    const burger = document.querySelector('.child');
+    const nav = document.querySelector('.content');
+
+    child.addEventListener('click', () => {
+        console.log('click')
+        nav.classList.toggle('content-active');
+        burger.classList.toggle('bak');
+    });
+}
+
+navSlide();*/
